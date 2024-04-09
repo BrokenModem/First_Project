ca506698328df2d9b6d9ebeea8dd9fec1c6265e8
BrokenModem <112692508+BrokenModem@users.noreply.github.com>
2024-04-09 10:35:14 +0200

    ye
    ye

---
+++ Source/First_Project/CaptureZone.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CaptureZone.h"
+
+// Sets default values
+ACaptureZone::ACaptureZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+
+	RootComponent = CollisionComponent;
+
+}
+
+// Called when the game starts or when spawned
+void ACaptureZone::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &ACaptureZone::OnOverlapBegin);
+	CollisionComponent->OnComponentEndOverlap.AddDynamic(this, &ACaptureZone::OnOverlapEnd);
+	
+}
+
+void ACaptureZone::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, .5f, FColor::Yellow, TEXT("Colliding"));
+	}
+}
+
+void ACaptureZone::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, .5f, FColor::Yellow, TEXT("Not Colliding"));
+	}
+}
+
+// Called every frame
+void ACaptureZone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
